e62d533e2d94d98cc2f0e6c0bbaf3a4d77c8a66e
John Phelan <johnjp999@gmail.com>
2021-06-13 16:57:37 -0400

    Added django-heroku

--- Corey_tut1/settings.py
+++ Corey_tut1/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -150,3 +151,5 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_S3_REGION_NAME= 'us-east-2'
+
+django_heroku.settings(locals())
